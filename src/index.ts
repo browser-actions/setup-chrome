8174b930b8d1cd5dbc549c194441b20e7d9abb45
Shin'ya UEOKA <ueokande@i-beam.org>
2020-12-28 06:30:42 +0000

    Add install-chromium script

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello, GitHub Actions!");
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as installer from "./installer";
+import { getPlatform } from "./platform";
+import path from "path";
+
+async function run(): Promise<void> {
+  try {
+    const version = core.getInput("chromium-version") || "latest";
+    const platform = getPlatform();
+
+    core.info(`Setup chromium ${version}`);
+
+    const installDir = await installer.install(platform, version);
+
+    core.addPath(path.join(installDir));
+    core.info(`Successfully setup chromium version ${version}`);
+
+    await exec.exec("chromium", ["--version"]);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
