d047f6e822135cbf3705726a07c58d834a51502b
Shin'ya Ueoka <ueokande@i-beam.org>
2024-05-26 07:08:37 +0000

    Support chromedriver at all

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,73 @@
 import path from "node:path";
 import * as core from "@actions/core";
 import * as exec from "@actions/exec";
+import { LinuxChannelInstaller } from "./channel_linux";
+import { MacOSChannelInstaller } from "./channel_macos";
+import { WindowsChannelInstaller } from "./channel_windows";
 import { installDependencies } from "./dependencies";
-import * as installer from "./installer";
+import type { Installer } from "./installer";
+import { LatestInstaller } from "./latest_installer";
 import { OS, type Platform, getPlatform } from "./platform";
+import { SnapshotInstaller } from "./snapshot_installer";
+import { parse } from "./version";
+import { KnownGoodVersionInstaller } from "./version_installer";
 
 const hasErrorMessage = (e: unknown): e is { message: string | Error } => {
   return typeof e === "object" && e !== null && "message" in e;
+};
+
+const getInstaller = (platform: Platform, version: string) => {
+  const spec = parse(version);
+  switch (spec.value.type) {
+    case "latest":
+      return new LatestInstaller(platform);
+    case "channel":
+      switch (platform.os) {
+        case OS.LINUX:
+          return new LinuxChannelInstaller(platform);
+        case OS.DARWIN:
+          return new MacOSChannelInstaller(platform);
+        case OS.WINDOWS:
+          return new WindowsChannelInstaller(platform);
+      }
+      break;
+    case "snapshot":
+      return new SnapshotInstaller(platform);
+    case "four-parts":
+      return new KnownGoodVersionInstaller(platform);
+  }
+};
+
+const installBrowser = async (installer: Installer, version: string) => {
+  const cache = await installer.checkInstalledBrowser(version);
+  if (cache) {
+    core.info(`Found in cache of chrome ${version} @ ${cache.root}`);
+    return path.join(cache.root, cache.bin);
+  }
+
+  core.info(`Attempting to download chrome ${version}...`);
+  const { archive } = await installer.downloadBrowser(version);
+
+  core.info("Installing chrome...");
+  const { root, bin } = await installer.installBrowser(version, archive);
+
+  return path.join(root, bin);
+};
+
+const installDriver = async (installer: Installer, version: string) => {
+  const cache = await installer.checkInstalledDriver(version);
+  if (cache) {
+    core.info(`Found in cache of chromedriver ${version} @ ${cache.root}`);
+    return path.join(cache.root, cache.bin);
+  }
+
+  core.info(`Attempting to download chromedriver ${version}...`);
+  const { archive } = await installer.downloadDriver(version);
+
+  core.info("Installing chromedriver...");
+  const { root, bin } = await installer.installDriver(version, archive);
+
+  return path.join(root, bin);
 };
 
 const testVersion = async (
@@ -29,18 +90,26 @@ const testVersion = async (
   const output = await exec.getExecOutput(`"${bin}"`, ["--version"], {});
   if (output.exitCode !== 0) {
     throw new Error(
-      `chromium exits with status ${output.exitCode}: ${output.stderr}`,
+      `${path.basename(bin)} exits with status ${output.exitCode}: ${
+        output.stderr
+      }`,
     );
   }
+
+  const stdout = output.stdout.trim();
   if (
-    !output.stdout.startsWith("Chromium ") &&
-    !output.stdout.startsWith("Google Chrome ")
+    !stdout.startsWith("Chromium ") &&
+    !stdout.startsWith("Google Chrome ") &&
+    !stdout.startsWith("ChromeDriver ")
   ) {
-    throw new Error(`chromium outputs unexpected results: ${output.stdout}`);
+    throw new Error(
+      `${path.basename(bin)} outputs unexpected results: ${stdout}`,
+    );
   }
-  return output.stdout
+  return stdout
     .replace("Chromium ", "")
     .replace("Google Chrome ", "")
+    .replace("ChromeDriver ", "")
     .split(" ", 1)[0];
 };
 
@@ -50,6 +119,8 @@ async function run(): Promise<void> {
     const platform = getPlatform();
     const flagInstallDependencies =
       core.getInput("install-dependencies") === "true";
+    const noInstallChromedriver =
+      core.getInput("no-install-chromedriver") === "true";
     const noSudo = core.getInput("no-sudo") === "true";
 
     if (flagInstallDependencies) {
@@ -57,19 +128,29 @@ async function run(): Promise<void> {
       await installDependencies(platform, { noSudo });
     }
 
-    core.info(`Setup chromium ${version}`);
+    core.info(`Setup chrome ${version}`);
 
-    const binPath = await installer.install(platform, version);
-    const installDir = path.dirname(binPath);
+    const installer = getInstaller(platform, version);
+    const browserBinPath = await installBrowser(installer, version);
+    const actualBrowserVersion = await testVersion(platform, browserBinPath);
 
-    core.addPath(path.join(installDir));
+    core.addPath(path.dirname(browserBinPath));
+    core.setOutput("chrome-path", browserBinPath);
+    core.setOutput("chrome-version", actualBrowserVersion);
+    core.info(`Successfully setup chromium ${actualBrowserVersion}`);
 
-    const actualVersion = await testVersion(platform, binPath);
+    if (!noInstallChromedriver) {
+      core.info(`Setup chromedriver ${version}`);
 
-    core.info(`Successfully setup chromium version ${actualVersion}`);
+      const driverInstaller = new SnapshotInstaller(platform);
+      const driverBinPath = await installDriver(driverInstaller, version);
+      const actualDriverVersion = await testVersion(platform, driverBinPath);
 
-    core.setOutput("chrome-version", actualVersion);
-    core.setOutput("chrome-path", binPath);
+      core.addPath(path.dirname(driverBinPath));
+      core.setOutput("chromedriver-path", driverBinPath);
+      core.setOutput("chromedriver-version", actualDriverVersion);
+      core.info(`Successfully setup chromedriver ${actualDriverVersion}`);
+    }
   } catch (error) {
     if (hasErrorMessage(error)) {
       core.setFailed(error.message);
--- src/installer.ts
+++ src/installer.ts
@@ -1,14 +1,3 @@
-import path from "node:path";
-import * as core from "@actions/core";
-import { LinuxChannelInstaller } from "./channel_linux";
-import { MacOSChannelInstaller } from "./channel_macos";
-import { WindowsChannelInstaller } from "./channel_windows";
-import { LatestInstaller } from "./latest_installer";
-import { OS, type Platform } from "./platform";
-import { SnapshotInstaller } from "./snapshot_installer";
-import { parse } from "./version";
-import { KnownGoodVersionInstaller } from "./version_installer";
-
 export type InstallResult = {
   root: string; // root is a directory containing all contents for chromium
   bin: string; // bin is a sub-path to chromium executable binary from root
@@ -27,44 +16,3 @@ export interface Installer {
   downloadDriver(version: string): Promise<DownloadResult>;
   installDriver(version: string, archive: string): Promise<InstallResult>;
 }
-
-export const install = async (
-  platform: Platform,
-  version: string,
-): Promise<string> => {
-  const installer = (() => {
-    const spec = parse(version);
-    switch (spec.value.type) {
-      case "latest":
-        return new LatestInstaller(platform);
-      case "channel":
-        switch (platform.os) {
-          case OS.LINUX:
-            return new LinuxChannelInstaller(platform);
-          case OS.DARWIN:
-            return new MacOSChannelInstaller(platform);
-          case OS.WINDOWS:
-            return new WindowsChannelInstaller(platform);
-        }
-        break;
-      case "snapshot":
-        return new SnapshotInstaller(platform);
-      case "four-parts":
-        return new KnownGoodVersionInstaller(platform);
-    }
-  })();
-
-  const cache = await installer.checkInstalledBrowser(version);
-  if (cache) {
-    core.info(`Found in cache @ ${cache.root}`);
-    return path.join(cache.root, cache.bin);
-  }
-
-  core.info(`Attempting to download ${version}...`);
-  const { archive } = await installer.downloadBrowser(version);
-
-  core.info("Installing chromium...");
-  const { root, bin } = await installer.installBrowser(version, archive);
-
-  return path.join(root, bin);
-};
