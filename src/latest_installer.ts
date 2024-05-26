3ffc657044a4ef01f764953ee49ac061b93f7144
Shin'ya Ueoka <ueokande@i-beam.org>
2024-05-26 06:44:12 +0000

    resolve snapshot version for latest and use it for cache key

--- src/latest_installer.ts
+++ src/latest_installer.ts
@@ -7,21 +7,47 @@ import { SnapshotInstaller } from "./snapshot_installer";
 export class LatestInstaller implements Installer {
   private readonly snapshotInstaller = new SnapshotInstaller(this.platform);
 
+  private latestSnapshotCache: string | undefined;
+
   constructor(private readonly platform: Platform) {}
 
-  async checkInstalled(version: string): Promise<InstallResult | undefined> {
-    const root = await cache.find("chromium", version);
-    if (root) {
-      return { root, bin: "chrome" };
-    }
+  async checkInstalled(_version: string): Promise<InstallResult | undefined> {
+    const snapshot = await this.getLatestSnapshot();
+    return this.snapshotInstaller.checkInstalled(snapshot);
   }
 
   async downloadBrowser(_version: string): Promise<DownloadResult> {
-    const version = await resolveLatestVersion(this.platform);
-    return this.snapshotInstaller.downloadBrowser(version);
+    const snapshot = await this.getLatestSnapshot();
+    return this.snapshotInstaller.downloadBrowser(snapshot);
   }
 
-  installBrowser = this.snapshotInstaller.installBrowser;
-  downloadDriver = this.snapshotInstaller.downloadDriver;
-  installDriver = this.snapshotInstaller.installDriver;
+  async installBrowser(
+    _version: string,
+    archive: string,
+  ): Promise<InstallResult> {
+    const snapshot = await this.getLatestSnapshot();
+    return this.snapshotInstaller.installBrowser(snapshot, archive);
+  }
+
+  async downloadDriver(_version: string): Promise<DownloadResult> {
+    const version = await this.getLatestSnapshot();
+    return this.snapshotInstaller.downloadDriver(version);
+  }
+
+  async installDriver(
+    _version: string,
+    archive: string,
+  ): Promise<InstallResult> {
+    const snapshot = await this.getLatestSnapshot();
+    return this.snapshotInstaller.installDriver(snapshot, archive);
+  }
+
+  private async getLatestSnapshot(): Promise<string> {
+    if (this.latestSnapshotCache) {
+      return Promise.resolve(this.latestSnapshotCache);
+    }
+
+    this.latestSnapshotCache = await resolveLatestVersion(this.platform);
+    return this.latestSnapshotCache;
+  }
 }
